e6202188e87a4bd0641a6b283d60a6410c8dea18
kartikey yadav <kartikeyy42@gmail.com>
2024-08-27 18:13:06 +0530

    posts crud operations

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from blogs.models import Category
-
+from blogs.models import Blog
 
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title', 'Category', 'featured_image', 'short_description', 'blog_body', 'status', 'is_featured')
+
